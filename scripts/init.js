28310e14aaf7f8bdbf3251d22ce6102fea887d4b
mark pearce <mark.pearce72@gmail.com>
2022-06-12 12:47:48 +0200

    Release for all systems

--- scripts/init.js
+++ scripts/init.js
@@ -1,35 +1,14 @@
-/*
-Hooks.on("init", function() {
-  console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
-});
-*/
+Hooks.once('ready', () => {
+  console.log('Combat-Tracker-Images | hooked in')
+})
 
-Hooks.once("ready", () => {
-  console.log("Combat-Tracker-Images | hooked in");
-  // Set up socket listener 
-  /*
-  game.socket.on("module.combat-tracker-images", (data) => {
-    console.log("hook" + data);
-  });
-  */
-});
-//import { updateCombatTrackerImages } from "./scripts.js";
-
-
-Hooks.on("createCombatant", function(combatant) {
-if (game.user.isGM){
-
-//  console.log (combatant.data._id);
-let actor = game.actors.get(combatant.data.actorId);
-//let flags = actor.data.flags;
-//console.log("flags: ", flags);
-let filepath = actor.getFlag('combat-tracker-images','trackerImage');
-if (filepath != undefined) {
-  let imgUpdate = [{_id:combatant.data._id,"img": filepath}];
-  game.combat.updateEmbeddedDocuments("Combatant", imgUpdate);
-}
-}
-});
-
-
-
+Hooks.on('createCombatant', function (combatant) {
+  if (game.user.isGM) {
+    let actor = game.actors.get(combatant.data.actorId)
+    let filepath = actor.getFlag('combat-tracker-images', 'trackerImage')
+    if (filepath != undefined) {
+      let imgUpdate = [{ _id: combatant.data._id, img: filepath }]
+      game.combat.updateEmbeddedDocuments('Combatant', imgUpdate)
+    }
+  }
+})
