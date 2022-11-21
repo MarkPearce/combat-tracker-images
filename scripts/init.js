6c4a424dc5cd4b6ff01449b6e5f61bad71719896
mark pearce <mark.pearce72@gmail.com>
2022-11-21 23:47:41 +0100

    update to v10

--- scripts/init.js
+++ scripts/init.js
@@ -4,10 +4,10 @@ Hooks.once('ready', () => {
 
 Hooks.on('createCombatant', function (combatant) {
   if (game.user.isGM) {
-    let actor = game.actors.get(combatant.data.actorId)
+    let actor = game.actors.get(combatant.actorId)
     let filepath = actor.getFlag('combat-tracker-images', 'trackerImage')
     if (filepath != undefined) {
-      let imgUpdate = [{ _id: combatant.data._id, img: filepath }]
+      let imgUpdate = [{ _id: combatant._id, img: filepath }]
       game.combat.updateEmbeddedDocuments('Combatant', imgUpdate)
     }
   }
